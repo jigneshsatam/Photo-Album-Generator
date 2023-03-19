d152f5259642b25535f804effcad95259532e9e0
JigneshSatam <jigneshsatam@gmail.com>
2023-03-19 02:40:09 -0500

    Updated packing of the application

--- packaging/mac-docker.sh
+++ packaging/mac-docker.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+install_docker() {
+
+}
+
+command_exists() {
+	command -v "$@" > /dev/null 2>&1
+}
+
+docker_running() {
+  "$@" > /dev/null 2>&1
+}
+
+if ! command_exists docker; then
+  install_docker
+fi
+
+if ! docker_running docker ps; then
+  echo "Starting docker..."
+  open -a "/Applications/Docker Desktop"
+  echo "Docker started..."
+fi
