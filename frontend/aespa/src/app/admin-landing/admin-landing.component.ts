90e9c70256679cf601e2b8fcc1daadd61c1ecce7
farishah <farishah.n1dfw@gmail.com>
2023-04-12 21:19:30 -0500

    Hardcoded UI for Album list
    I will add the python function to connect this UI to the postgre directory list, in my separate 3.2 branch

--- frontend/aespa/src/app/admin-landing/admin-landing.component.ts
+++ frontend/aespa/src/app/admin-landing/admin-landing.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-admin-landing',
   templateUrl: './admin-landing.component.html',
-  styleUrls: ['./admin-landing.component.sass']
+  styleUrls: ['./admin-landing.component.css']
 })
+
 export class AdminLandingComponent {
 
 }
