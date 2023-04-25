673890b55677398f7fdee545fc4d3d5ac1de4a6e
jacobvillegas <jacobvillegas@gmail.com>
2023-04-25 18:51:30 -0500

    tag model

--- postgres-sql/create-tables.sql
+++ postgres-sql/create-tables.sql
@@ -19,5 +19,27 @@ CREATE TABLE IF NOT EXISTS imgdirectories
         ON DELETE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS photos(
+  photo_id SERIAL PRIMARY KEY,
+  photo_directory TEXT NOT NULL,
+  photo_path TEXT NOT NULL,
+  user_id  INTEGER UNIQUE NOT NULL,
+  tag_id  INTERGER UNIQUE,
+  CONSTRAINT FK_userinfo FOREIGN KEY(user_id) REFERENCES userinfo(id) ON DELETE CASCADE,
+  CONSTRAINT FK_imgdirectories FOREIGN KEY(photo_directory) REFERENCES imgdirectories(dirpath),
+  CONSTRAINT FK_tags FOREIGN KEY(tag_id) REFERENCES tags(tag_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS tags(
+  tag_id SERIAL PRIMARY KEY,
+  tag TEXT NOT NULL,
+  user_id INTEGER UNIQUE NOT NULL,
+  foto_id INTEGER UNIQUE NOT NULL,
+  foto_dir TEXT NOT NULL,
+  CONSTRAINT FK_userifno FOREIGN KEY(user_id) REFERENCES userinfo(id) ON DELETE CASCADE,
+  CONSTRAINT FK_photos FOREIGN KEY(foto_id) REFERENCES photos(photo_id) ON DELETE CASCADE,
+  CONSTRAINT FK_photos FOREIGN KEY(foto_dir) REFERENCES photos(photo_directory) ON DELETE CASCADE    
+);
+
 INSERT INTO userinfo(username, pwd, firstname, lastname, usertype)
 VALUES('defaultuser', 'password', 'Default', 'User', 'admin');
