1b8d5db6114891a45ba5cd13d6d7f5223d10ca4f
farishah <farishah.n1dfw@gmail.com>
2023-04-30 18:52:10 -0500

    Add 2 new buttons on Admin Landing, fixed the tag id incrementing small issue

--- backend/flaskr/tag_management/tag_model.py
+++ backend/flaskr/tag_management/tag_model.py
@@ -4,47 +4,57 @@ import logging
 class Tag:
     def create_tag(tags):
         result = False
-
         try:
             conn = Connect().get_connection()
-           
             cursor = conn.cursor()
+            getTagsQuery = "SELECT tag FROM tag" #executing in line 14
 
-            # for tag in tags:
-            #     cursor.execute(
-            #     "select * from tag"
-            #     "  where name = ' " + tag + " ' ")
-            #     print(tag)
-
-            def stringify(v): 
-                return "('%s')" % (v)
-
-            #transform all to string
-            v = map(stringify, tags)
-
-            #glue them together
-            batchData = ", ".join(e for e in v)
-
-            # sql = "INSERT INTO tag (tag) \
-            # VALUES %s" % batchData 
-            # "ON CONFLICT (tag) DO NOTHING"
-            
-            sql = "INSERT INTO tag (tag) \
-                  VALUES %s \
-                  ON CONFLICT (tag) DO NOTHING" % batchData
-            
+            #When new tag is added, it checks duplicates and SQL querying to database to get list of tags
             cursor = conn.cursor()
-            cursor.execute(sql)
+            cursor.execute(getTagsQuery) 
+            #get all tag names in tagRows variable
+            tagRows = cursor.fetchall() 
+            #when we get data from db, I am restructuring as an array
+            tagLists = [entry for entry, in tagRows]
+            #defined unique tags array (blank)
+            uniqueTags = []
+            #running for loop on tag list
+            for element in tags:
+                #check if tag sent by used is in db or not
+                #if tag is not in tag list, then it will push that tag in the unique tags array
+                if element not in tagLists:
+                    #element checks tags one by one
+                    uniqueTags.append(element)
+            #adding for logging purposes
+            print('uniqueTags to Insert Are -->>>', uniqueTags)
+            print('Tags Sent By User', tags)
+            if len(uniqueTags):
+                #in a single query, it sends all the data
+                # v = string 
+                def stringify(tagstring): 
+                    return "('%s')" % (tagstring)
+
+                #transform all to string
+                #pass down all unique tags in a string format
+                tagstring = map(stringify, uniqueTags)
+                #glue them together
+                batchData = ", ".join(e for e in tagstring)
+
+                print('Batch Query To insert SQL', batchData)
+                # Insert Batch Unique Tags Data
+
+                #batch data was generated from stringify function
+                sql = "insert into tag (tag) \
+                VALUES %s" % batchData
+                cursor.execute(sql)
             conn.commit()
-
             cursor.close()
-
             result = True
         except Exception as e:
             logging.error(e)
-            result = True        
+            result = False        
 
-        return 1, result
+        return result
     
     def get_tags():
         result = False
