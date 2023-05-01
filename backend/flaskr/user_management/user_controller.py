c4100f5d85122e69f5e970fcd9fd292b6a373e1c
Jignesh Satam <jigneshsatam@gmail.com>
2023-04-30 19:06:49 -0500

    Revert "Error util + handling"

--- backend/flaskr/user_management/user_controller.py
+++ backend/flaskr/user_management/user_controller.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, Response, jsonify, request
 from .user_model import User
-from util.error_util import ErrorUtil
 
 users_routes = Blueprint("users_routes", __name__, url_prefix="/users")
 
@@ -9,31 +8,31 @@ users_routes = Blueprint("users_routes", __name__, url_prefix="/users")
 def register_user():
     # Check if data is provided in request
     if not request.data:
-        return ErrorUtil.get_json_response(ErrorUtil.JSON_DATA_MISSING)
+        return jsonify({'status': 'JSON data is missing'}), 404
 
     # Get username from request
     if str(request.json.get('userName')) != 'None':
         user_name = str(request.json.get('userName'))
     else:
-        return ErrorUtil.get_json_response(ErrorUtil.NO_USER_NAME)
+        return jsonify({'status': 'username is missing'}), 404
 
     # Get password from request
     if str(request.json.get('pwd')) != 'None':
         password = str(request.json.get('pwd'))
     else:
-        return ErrorUtil.get_json_response(ErrorUtil.NO_PASSWORD)
+        return jsonify({'status': 'password is missing'}), 404
 
     # Get first name from request
     if str(request.json.get('fName')) != 'None':
         first_name = str(request.json.get('fName'))
     else:
-        return ErrorUtil.get_json_response(ErrorUtil.NO_FIRST_NAME)
+        return jsonify({'status': 'first name is missing'}), 404
 
     # Get last name from request
     if str(request.json.get('lName')) != 'None':
         last_name = str(request.json.get('lName'))
     else:
-        return ErrorUtil.get_json_response(ErrorUtil.NO_LAST_NAME)
+        return jsonify({'status': 'last name is missing'}), 404
 
     # Get user type from request
     user_type = str(request.json.get('userType')) if str(request.json.get('userType')) != "None" else "admin"
