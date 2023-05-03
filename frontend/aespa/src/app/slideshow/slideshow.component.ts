c62d40d81beaaac80321c2c308f3f2aebfb3d284
Ricky Padilla <rpadilla@Rickys-MacBook-Air.local>
2023-05-03 16:18:50 -0500

    add slideshow logic

--- frontend/aespa/src/app/slideshow/slideshow.component.ts
+++ frontend/aespa/src/app/slideshow/slideshow.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface SlideshowRouterState {
+  images: any[];
+}
 
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.sass']
 })
-export class SlideshowComponent {
+export class SlideshowComponent implements OnInit {
+  images: any[] = [];
 
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    if (navigation && navigation.extras.state) {
+      const state = navigation.extras.state as SlideshowRouterState;
+      this.images = state.images;
+    }
+  }
+
+  ngOnInit(): void {
+  }
 }
