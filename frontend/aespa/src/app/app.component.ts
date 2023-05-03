74cd4498521297e0b43b04898ab132e327037241
mmrepos <michaelmmckenzie11@gmail.com>
2023-05-03 08:24:27 -0500

    Updated app.component.html and app.component.ts for logout

--- frontend/aespa/src/app/app.component.ts
+++ frontend/aespa/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from './_services/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) { }
   title = 'aespa';
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  isLoggedIn() {
+    return this.authenticationService.currentUserValue;
+  }
+  
 }
