9cedc6f01fcda330813df476ae2794ed81c7ae18
mmrepos <michaelmmckenzie11@gmail.com>
2023-05-04 08:55:44 -0500

    Added login API in backend

---
+++ backend/flaskr/__init__.py
@@ -0,0 +1,52 @@
+import os
+
+from flask import Flask
+from flask_cors import CORS
+
+from . import db
+from .image_management.image_controller import images_routes
+from .user_management.user_controller import users_routes
+from .login_management.login_controller import login_routes
+from .cache.session_store import SessionStore
+
+
+def create_app(test_config=None):
+  # create and configure the app
+  app = Flask(__name__, instance_relative_config=True)
+  CORS(app)
+  app.config.from_mapping(
+      SECRET_KEY='dev',
+      DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+  )
+
+  session_store = SessionStore(host='cache', port=6379)
+
+  if test_config is None:
+    # load the instance config, if it exists, when not testing
+    app.config.from_pyfile('config.py', silent=True)
+  else:
+    # load the test config if passed in
+    app.config.from_mapping(test_config)
+
+  # ensure the instance folder exists
+  try:
+    os.makedirs(app.instance_path)
+  except OSError:
+    pass
+
+  # a simple page that says hello
+
+  @app.route('/')
+  def hello() -> str:
+    count = session_store.get_hit_count()
+    # count = 0
+
+    return 'Hello World! I have been seen {} times.\n'.format(count)
+
+  app.register_blueprint(images_routes)
+  app.register_blueprint(login_routes)
+  app.register_blueprint(users_routes)
+
+  #db.init_app(app)
+
+  return app
---
+++ backend/flaskr/user_management/user_model.py
@@ -0,0 +1,31 @@
+from flaskr.db.postgres_db_connect import Connect
+import logging
+
+class User:
+    def create_user(user_name, password, first_name, last_name, user_type):
+        result = False
+
+        try:
+            conn = Connect().get_connection()
+
+            # Create cursor to perform database operations
+            cursor = conn.cursor()
+
+            cursor.execute(
+            "insert into UserInfo(userName, pwd, firstName, lastName, userType)"
+            " values('" + user_name + "', '" + password + "', '" + first_name + "', '" + last_name +
+            "', '" + user_type + "') returning id")
+
+            # Get id for new directory path
+            user_id = int(cursor.fetchone()[0])
+
+            conn.commit()
+
+            cursor.close()
+
+            result = True
+        except Exception as e:
+            logging.error(e)
+            result = False        
+
+        return user_id, result
